9c219a53cb2008dc02d50d8e6c7a1dfec1c5aa2a
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2022-05-19 13:55:15 +0530

    v3.0

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(400, 350);
@@ -11,10 +16,19 @@ function modelLoaded() {
 }
 function draw() {
     background('#9C63F8');
+    textSize(difference);
+    fill('#76DEC4');
+    text('text',noseX,noseY);
     
 }
 function gotPoses(r) {
     if(r.length > 0) {
         console.log(r);
+        noseX = r[0].pose.nose.x;
+        noseY = r[0].pose.nose.y;
+        leftWristX = r[0].pose.leftWrist.x;
+        rightWristX = r[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist " + leftWristX + " Right wrist " + rightWristX + " Difference " + difference);
     }
 }
